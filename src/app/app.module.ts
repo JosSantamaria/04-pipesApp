882e37def1b9e1f60c500cbc792fd23b2dadd6c0
Jos Santamaria <josetsantamaria@gmail.com>
2022-03-07 17:43:44 -0600

    componentes basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 // Mod :: Personalizado Prime Ng Module (all imports)
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule,
     
   ],
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -16,30 +16,42 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.items =
-    [ 
+    [
+      {label:'Pipes de Angular',
+        icon:'pi pi-desktop',
+        items:
+        [
+          {
+            label:'Textos y Fechas',
+            icon:'pi pi-align-left',
+            routerLink:' '
+          },
+          {
+            label:'Numeros',
+            icon:'pi pi-dollar',
+            routerLink:'numeros'
+          },
+          {
+            label:'No Comunes',
+            icon:'pi pi-pound',
+            routerLink:'no-comunes'
+          }
+        ]
+       
+      },
       {
-        label: 'File',
-        items: [{
-                label: 'New', 
-                icon: 'pi pi-fw pi-plus',
-                items: [
-                    {label: 'Project'},
-                    {label: 'Other'},
-                ]
-            },
-            {label: 'Open'},
-            {label: 'Quit'}
+        label:'Pipes Personalizados',
+        icon:'pi pi-cog',
+        items:
+        [
+          {
+            label:'Textos',
+            icon:'pi pi-align-left',
+            routerLink:' '
+          }
         ]
-    },
-    {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-            {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-            {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-        ]
-    }
-
+      }
+      
     ]
     
   }
