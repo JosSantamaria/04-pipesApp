834d878d181fdbd3e0e84e59e688bc96f38232b2
Jos Santamaria <josetsantamaria@gmail.com>
2022-03-08 11:08:29 -0600

    pipes-primeflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { AppRouterModule } from './app-router.module';
     BrowserModule,
     AppRouterModule,
     SharedModule,
+    VentasModule
     
   ],
   providers: [],
